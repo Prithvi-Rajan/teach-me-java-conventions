cad5d53f3e49a6e95e7bd12eae84591d56662ccb
Prithvi <karthikprithvi811@gmail.com>
2022-03-11 12:41:30 +0530

    Fixed method names and added a test case for 1^0=1

--- src/test/java/PowerFinderTest.java
+++ src/test/java/PowerFinderTest.java
@@ -3,22 +3,27 @@ import powerpackage.PowerFinder;
 
 class PowerFinderTest {
     @Test
-    public void one_raised_to_one_is_one_is_1() {
+    public void onePowerZeroIsOne() {
+        assert PowerFinder.of(1,0) == 1;
+    }
+
+    @Test
+    public void onePowerOneIsOne() {
         assert PowerFinder.of(1,1) == 1;
     }
 
     @Test
-    public void two_raised_to_one_is_2() {
+    public void twoPowerOneIsTwo() {
         assert PowerFinder.of(2,1) == 2;
     }
 
     @Test
-    public void two_raised_to_two_is_4() {
+    public void twoPowerTwoIsFour() {
         assert PowerFinder.of(2, 2) == 4;
     }
 
     @Test
-    public void three_raised_to_two_is_9() {
+    public void threePowerTwoIsNine() {
         assert PowerFinder.of(3, 2) == 3*3;
     }
 }
